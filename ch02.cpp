0a21279897ca6c7e7bc2cc624e1c425bfa9dfcc3
jing-koo <70867791+jing-koo@users.noreply.github.com>
2020-09-07 12:08:53 +0800

    Add files via upload

---
+++ ch02.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	
+	int a,b,c;
+	
+	a=1;
+	b=2;
+	c=3; 
+	
+	int d=4,e=5,f=6;
+	
+    cout<<"a="<<a <<",b="<<b <<",c="<<c<<endl;
+	cout<<"d="<<d <<",e="<<e <<",f="<<f<<endl;
+	
+	return 0;
+	
+}
